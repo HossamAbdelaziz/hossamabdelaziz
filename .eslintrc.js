2bc743bb9b139ea87eb6adde8d081172ef5e0470
hossam abdelaziz <hossam.anwar.aziz@gmail.com>
2025-06-18 16:48:09 -0400

    Disable ESLint rules for unescaped text and unused vars

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+    extends: 'next',
+    rules: {
+        'react/no-unescaped-entities': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+    },
+};
